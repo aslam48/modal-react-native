710e1037a8b5e62a7eb4ae4c3b1ea85365216ef6
aslam48 <youngfizzy001@gmail.com>
2023-04-13 12:26:11 +0100

    working

--- App.js
+++ App.js
@@ -6,12 +6,16 @@ import {
   Button,
   StyleSheet,
   Modal,
+  TouchableOpacity,
+  Image,
 } from "react-native";
+
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { styles } from "./style";
 import { useState } from "react";
-
+import Down from './assets/down.png'
+import usFlag from './assets/us-flag.jpg'
 export default function App() {
   const [visible, setVisibility] = useState(false);
   const show = () => setVisibility(true);
@@ -27,39 +31,61 @@ export default function App() {
       >
         <Pressable style={styles.upper} onPress={hide} />
         <View style={styles.lower}>
+          <View style={styles.arror} onPress={hide}>
+            <Ionicons name="arrow-back-sharp" size={30} color="black" />
+          </View>
 
-     
-        <View style={styles.arror}>              
-            <Ionicons name="arrow-back-sharp" size={30} color="black" onPress={hide}/>
-         </View> 
+          <View style={styles.componentTitle}>
+            <Text style={styles.swapText}>Continue Swap</Text>
+          </View>
 
-           <View style={styles.componentTitle}>
-              <Text style={styles.swapText}>Continue Swap</Text>
-            </View>
-
-
-
-              {/* create swup details  */}
-           <View style={styles.centerItem}>
-
+          {/* create swup details  */}
+          <View style={styles.centerItem}>
             <View style={styles.containerColume}>
-            <View style={styles.titleSwap}>
+              <View>
                 <Text style={styles.swupText}>Create Swap Order</Text>
+              </View>
+
+              <View>
+                <Text style={styles.centerSubText}>
+                  Enter the quantity you want
+                </Text>
+                <Text style={styles.centerSubText}>below</Text>
+                <Text style={styles.centerSubText__Quoted__Rate}>
+                  Quoted Rate
+                </Text>
+
+                {/* rate  */}
+                <View style={styles.rateBtn}>
+                <TouchableOpacity 
+                 activeOpacity={1} style={styles.rate}>
+                  <Text style={styles.rateText}>700</Text>
+                </TouchableOpacity>
+                </View>
+
+                {/* input  */}
+                <View style={{flexDirection: "row"}}>
+                  <TouchableOpacity style={{width: 100, height: 50, marginHorizontal:5, borderBottomColor: "#111", borderBottomWidth: 1, flexDirection: "row", fontSize:12}}>
+                    
+                 <View style={{justifyContent: "center"}}>
+                  <Image source={Down}
+                  style={{width:12, height:10, tintColor: "#111"}}
+                  />
+                </View>
+
+                <View style={{justifyContent: "center", marginLeft: 5}}>
+                  <Image source={usFlag}
+                  resizeMode="contain" 
+                  style={{width: 35, height:13}}
+                  />
+                </View>
+                  </TouchableOpacity>
+                </View>
+
+
+              </View>
             </View>
-
-            <View>
-              <Text>Enter the quantity you want</Text>
-              <Text style={styles.center}>below</Text>
-            </View>
-
-            </View>
-           
-           </View>
-            
-
-
-           
-          
+          </View>
         </View>
       </Modal>
     </SafeAreaView>
--- style.js
+++ style.js
@@ -5,25 +5,32 @@ import {StyleSheet} from 'react-native'
  export const styles = StyleSheet.create({
             //  center Text
     centerItem: {flex: 1, justifyContent: 'center', flexDirection: "row"},
-            //  center Text
+    containerColume: {flexDirection: "column"},
+         
             
 
 
              //    title style 
     fill: {flex: 1},
     upper: {height: 100, backgroundColor: '#DDD', opacity: .5},
-    lower: {flex: 1, backgroundColor: "white" },
+    lower: {flex: 1, backgroundColor: "white",  borderRadius: 10,borderWidth: 1,borderColor: "#DDD"},
+
+
     arror: {position: "absolute", marginLeft: 10, marginTop: 8},
     componentTitle: {flex: 1, justifyContent: 'center', flexDirection: "row", marginTop: 13},
     swapText: {fontSize: 18, fontWeight: "bold"},
-             //    title style 
-                      
 
+                //   modal text 
+     swupText: {fontSize: 16, fontWeight: "bold",textAlign: 'center', marginBottom:17},
+     center: {justifyContent: 'center', flexDirection: "row"},
+     centerSubText: {textAlign: "center", color: "#707EAE"},
+     centerSubText__Quoted__Rate: {textAlign: "center", color: "#A9A9A9"},
+        
 
-    //  titleSwap: {flex: 1, justifyContent: 'center', flexDirection: "row"},
-     swupText: {fontSize: 15, fontWeight: "bold"},
-     containerColume: {flexDirection: "column"},
+     //  rate 
+ rate: {justifyContent:"center", width: 76, alignItems: "center", elevation: 2, backgroundColor: "#009688", borderRadius: 10, paddingVertical: 9, paddingHorizontal: 9},
+ rateText: {color: "white"},
+ rateBtn: {justifyContent: "center", alignItems: "center", marginTop: 10},
 
-     center: {justifyContent: 'center', flexDirection: "row"}
-
+ 
 })
