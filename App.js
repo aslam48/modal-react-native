055424d60d10841f3ce7b0c5264f3ddfbdfb60ec
aslam48 <youngfizzy001@gmail.com>
2023-04-14 08:55:34 +0100

    working

--- App.js
+++ App.js
@@ -8,40 +8,78 @@ import {
   Modal,
   TouchableOpacity,
   Image,
+  TextInput,
+  TouchableWithoutFeedback,
+  Dimensions,
+  
 } from "react-native";
 
+const {width, height} = Dimensions.get("window")
+
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
+import React, { useEffect } from "react";
 import { styles } from "./style";
 import { useState } from "react";
 import Down from './assets/down.png'
 import usFlag from './assets/us-flag.jpg'
+import Nigeria from './assets/Flag-Nigeria.webp'
+import CountryPicker from "react-native-country-picker-modal"
+import { FlatList } from "react-native";
+
+
 export default function App() {
-  const [visible, setVisibility] = useState(false);
-  const show = () => setVisibility(true);
-  const hide = () => setVisibility(false);
+
+//     const [currencies, setCurrencies] = useState([])
+//     const [selectedcurrencies, setSelectedCurrencies] = useState(null)
+//     const [modalVisible, setModalVisible] = useState(false)
+
+// useEffect(() => {
+//   fetch("https://restcountries.com/v3.1/all")
+//     .then(response => response.json())
+//     .then(data => {
+//       let currencyData = data.map(item => {
+//         return {
+//           code: item.appha2code,
+//           item: item.name,
+//           callingCode: `+${item.callingCodes[0]}`,
+//           flag: `https://countryflagsapi.com/png/${item.name}`
+//         }
+//       });
+//       setCurrencies(currencyData);
+//       if(currencyData.length > 0 ){
+//         let defaultData = areaData.filter(a =>a.code == "US")
+
+//         if(defaultData.length > 0){
+//           setSelectedCurrencies(defaultData[0])
+//         }
+//       }
+//     })
+// }, [])
+  
+  
+// function renderAreaCurrencyModal(){
+//   return(
+//     <Modal animationType="slide" transparent={true} visible={modalVisible}>
+//       <TouchableWithoutFeedback
+//       onPress={()=> setModalVisible(false)}
+//       >
+//         <View style={{flex:1, alignItems: "center", justifyContent: "center"}}>
+//           <View style={{height:400, width:width*0.8, backgroundColor: "#342342", borderRadius:12}}>
+//             <FlatList data={areas} renderItem={renderItem} keyExtractor={(item)=> item.code} showsVerticalScrollIndicator={false}/>
+//           </View>
+//         </View>
+//       </TouchableWithoutFeedback>
+//     </Modal>
+//   )
+// }
+
   return (
     <SafeAreaView style={styles.fill}>
-      <Button title="Show" onPress={show} />
-      <Modal
-        visible={visible}
-        onRequestClose={hide}
-        animationType="fade"
-        transparent
-      >
-        <Pressable style={styles.upper} onPress={hide} />
-        <View style={styles.lower}>
-          <View style={styles.arror} onPress={hide}>
-            <Ionicons name="arrow-back-sharp" size={30} color="black" />
-          </View>
 
-          <View style={styles.componentTitle}>
-            <Text style={styles.swapText}>Continue Swap</Text>
-          </View>
+         
 
           {/* create swup details  */}
-          <View style={styles.centerItem}>
-            <View style={styles.containerColume}>
+           
               <View>
                 <Text style={styles.swupText}>Create Swap Order</Text>
               </View>
@@ -62,32 +100,76 @@ export default function App() {
                   <Text style={styles.rateText}>700</Text>
                 </TouchableOpacity>
                 </View>
+              </View>
 
                 {/* input  */}
-                <View style={{flexDirection: "row"}}>
-                  <TouchableOpacity style={{width: 100, height: 50, marginHorizontal:5, borderBottomColor: "#111", borderBottomWidth: 1, flexDirection: "row", fontSize:12}}>
+                <View style={{flexDirection: "row", justifyContent:"center",}}>
+                  {/* amount text input */}
+                  <TextInput keyboardType="numeric" placeholder="Enter your amount" placeholderTextColor='gray' selectionColor='gray' style={{width: 200, marginVertical: 10, borderBottomColor:" #111", borderBottomWidth: 1, height: 40, fontSize: 20, color: "#111"}}  />
+
+
+                  <TouchableOpacity style={{width:80, height: 50, marginHorizontal:5, borderBottomColor: "#111", borderBottomWidth: 1, flexDirection: "row", fontSize:12}}>
                     
-                 <View style={{justifyContent: "center"}}>
-                  <Image source={Down}
-                  style={{width:12, height:10, tintColor: "#111"}}
-                  />
+                    <View style={{justifyContent: "center"}}>
+                     <Image source={Down}
+                     style={{width:12, height:10, tintColor: "#111"}}
+                     />
+                   </View>
+   
+                   <View style={{justifyContent: "center", marginLeft: 5}}>
+                     <Image source={usFlag}
+                     resizeMode="contain" 
+                     style={{width: 35, height:13}}
+                     />
+                     {/* <CountryPicker 
+                     withFilter
+                     withCurrency
+                     /> */}
+                   </View>
+   
+                   <View style={{justifyContent: "center", marginLeft: 5}}>
+                     <Text style={{ color: "#111", fontSize:14}}>usd</Text>
+                   </View>
+                     </TouchableOpacity>
+
+                     {/* {renderAreaCurrencyModal()} */}
                 </View>
 
-                <View style={{justifyContent: "center", marginLeft: 5}}>
-                  <Image source={usFlag}
-                  resizeMode="contain" 
-                  style={{width: 35, height:13}}
-                  />
-                </View>
-                  </TouchableOpacity>
-                </View>
+                {/* input 2 */}
+
+                <View style={{flexDirection: "row", justifyContent:"center",}}>
+                  {/* amount text input */}
+                  <TextInput keyboardType="numeric" placeholder="Enter your amount" placeholderTextColor='gray' selectionColor='gray' style={{width: 200, marginVertical: 10, borderBottomColor:" #111", borderBottomWidth: 1, height: 40, fontSize: 20, color: "#111"}}  />
 
 
-              </View>
-            </View>
-          </View>
-        </View>
-      </Modal>
+                  <TouchableOpacity style={{width:80, height: 50, marginHorizontal:5, borderBottomColor: "#111", borderBottomWidth: 1, flexDirection: "row", fontSize:12}}>
+                    
+                    <View style={{justifyContent: "center"}}>
+                     <Image source={Down}
+                     style={{width:12, height:10, tintColor: "#111"}}
+                     />
+                   </View>
+   
+                   <View style={{justifyContent: "center", marginLeft: 5}}>
+                     <Image source={Nigeria}
+                     resizeMode="contain" 
+                     style={{width: 35, height:13}}
+                     />
+                     {/* <CountryPicker 
+                     withFilter
+                     withCurrency
+                     /> */}
+                   </View>
+   
+                   <View style={{justifyContent: "center", marginLeft: 5}}>
+                     <Text style={{ color: "#111", fontSize:14}}>usd</Text>
+                   </View>
+                     </TouchableOpacity>
+
+                     {/* {renderAreaCurrencyModal()} */}
+                </View>
+           
+
     </SafeAreaView>
   );
 }
--- style.js
+++ style.js
@@ -11,14 +11,10 @@ import {StyleSheet} from 'react-native'
 
 
              //    title style 
-    fill: {flex: 1},
-    upper: {height: 100, backgroundColor: '#DDD', opacity: .5},
-    lower: {flex: 1, backgroundColor: "white",  borderRadius: 10,borderWidth: 1,borderColor: "#DDD"},
+    fill: {flex: 1, justifyContent: "center", alignItems:"center"},
 
 
-    arror: {position: "absolute", marginLeft: 10, marginTop: 8},
-    componentTitle: {flex: 1, justifyContent: 'center', flexDirection: "row", marginTop: 13},
-    swapText: {fontSize: 18, fontWeight: "bold"},
+  
 
                 //   modal text 
      swupText: {fontSize: 16, fontWeight: "bold",textAlign: 'center', marginBottom:17},
